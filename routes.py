697717e50acea4eb42b20c1cc9f0982b66b01c38
Row0 <Row0@outlook.com>
2023-08-03 02:04:39 +0800

    Updated db schema

--- routes.py
+++ routes.py
@@ -12,12 +12,18 @@ def index():
     db.session.add(PlaysIn(
         team_num=form.get("team_num"),
         game_id=form.get("game_id"),
-        auto_shoots=form.get("auto_shoots"),
-        auto_goals=form.get("auto_goals"),
-        teleop_shoots=form.get("teleop_shoots"),
-        teleop_goals=form.get("teleop_goals"),
-        climb_score=form.get("climb_score"),
-        foul=form.get("foul")
+        taxi_tarmac=True if form.get("taxi_tarmac") == "on" else False,
+        auto_upper=form.get("auto_upper"),
+        auto_upper_fail=form.get("auto_upper_fail"),
+        auto_lower=form.get("auto_lower"),
+        teleop_upper=form.get("teleop_upper"),
+        teleop_upper_fail=form.get("teleop_upper_fail"),
+        teleop_lower=form.get("teleop_lower"),
+        climb_rung=form.get("climb_rung"),
+        climb_fail=True if form.get("climb_fail") == "on" else False,
+        climb_pos=form.get("climb_pos"),
+        foul=form.get("foul"),
+        comments=form.get("comments")
     ))
     db.session.commit()
     return render_template("success.html")
