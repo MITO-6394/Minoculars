08c120f02e07635045f0f3a6e96589a90f0fb3c5
Row0 <Row0@outlook.com>
2023-08-03 19:31:52 +0800

    Deleted create_app()

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask
+
+
+app = Flask(__name__, template_folder="templates", static_folder="static")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+
+
 from routes import *
 
 
-def create_app():
-    app = Flask(__name__, template_folder="templates", static_folder="static")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-    return app
-
-
 if __name__ == "__main__":
-    app = create_app()
     app.run(port=9999, debug=True)
-
-
