aaeb3ebd0e718d53afeaf137df7b4b170422175b
Row0 <Row0@outlook.com>
2023-08-03 19:28:28 +0800

    Added create_app()

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
-
-
-app = Flask(__name__, template_folder="templates", static_folder="static")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
 from routes import *
 
+
+def create_app():
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    return app
+
+
 if __name__ == "__main__":
+    app = create_app()
     app.run(port=9999, debug=True)
+
+
